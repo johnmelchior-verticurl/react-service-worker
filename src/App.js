f126d3625eebbcbdc6e77f4187c122e14b5fb168
johnmelchior-verticurl <johnmelchior.arockiasamy@verticurl.com>
2021-10-08 15:39:33 +0530

    Firebase push notification resolve link errors

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
-import firebase from './firebase';
 import { getMessaging, getToken } from "firebase/messaging";
 
 
