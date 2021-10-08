78957062d8fa4063226d16ea4708d992f637e320
johnmelchior-verticurl <johnmelchior.arockiasamy@verticurl.com>
2021-10-08 15:48:59 +0530

    Resolved PWA web push notification errors

--- src/App.js
+++ src/App.js
@@ -3,8 +3,19 @@ import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 import { getMessaging, getToken } from "firebase/messaging";
+import * as firebase from 'firebase/app';
 
+const firebaseConfig = {
+  apiKey: "AIzaSyD8B6H5wP7WmrWo4V--Ym-Z75U0q8cTIN8",
+  authDomain: "react-push-message-poc.firebaseapp.com",
+  projectId: "react-push-message-poc",
+  storageBucket: "react-push-message-poc.appspot.com",
+  messagingSenderId: "418084875562",
+  appId: "1:418084875562:web:c8f4200601e2e2788ec253"
+};
 
+firebase.initializeApp(firebaseConfig)
+console.log("Firebase", firebase);
 
 const App = () => {
 
--- src/firebase.js
+++ src/firebase.js
@@ -24,18 +24,4 @@
 // firebase.initializeApp(firebaseConfig);
 // export default firebase;
 
-import * as firebase from 'firebase/app';
 
-
-const firebaseConfig = {
-	apiKey: "AIzaSyD8B6H5wP7WmrWo4V--Ym-Z75U0q8cTIN8",
-	authDomain: "react-push-message-poc.firebaseapp.com",
-	projectId: "react-push-message-poc",
-	storageBucket: "react-push-message-poc.appspot.com",
-	messagingSenderId: "418084875562",
-	appId: "1:418084875562:web:c8f4200601e2e2788ec253"
-};
-
-firebase.initializeApp(firebaseConfig);
-
-export default firebase;
